567d2ee238dd7c24581d024348b499baec43c0b0
Sven Augustin <sven.augustin@psi.ch>
2020-10-01 20:29:01 +0200

    typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="sanipy",
     version="0.0.1",
-    url="https://github.com/augustin_s/sanipy",
+    url="https://gitlab.psi.ch/augustin_s/sanipy",
     description="A command-line tool for epics connection testing",
     author="Paul Scherrer Institute",
     packages=find_packages(),
