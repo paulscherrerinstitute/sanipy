9096961de625e8825b10e1c8f11206c1ee66ef7c
Sven Augustin <sven.augustin@psi.ch>
2020-11-02 14:43:20 +0100

    parallel/serial execute accepts several arguments now

--- utils/execute.py
+++ utils/execute.py
@@ -1,20 +1,21 @@
 from concurrent.futures import ThreadPoolExecutor
 
 
-def parallel(func, targets, names=None):
+def parallel(func, *targets, names=None):
     with ThreadPoolExecutor() as executor:
-        results = executor.map(func, targets)
-        if names:
+        results = executor.map(func, *targets)
+        if names is not None:
             return dict(zip(names, results))
         else:
             return list(results)
 
 
-def serial(func, targets, names=None):
-    if names:
-        return {n: func(t) for n, t in zip(names, targets)}
+def serial(func, *targets, names=None):
+    targets = zip(*targets)
+    if names is not None:
+        return {n: func(*t) for n, t in zip(names, targets)}
     else:
-        return [func(t) for t in targets]
+        return [func(*t) for t in targets]
 
 
 
