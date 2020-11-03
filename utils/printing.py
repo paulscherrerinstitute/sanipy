9f3a036b2e29af04e14117a0788445659bf02556
Sven Augustin <sven.augustin@psi.ch>
2020-11-03 14:00:09 +0100

    added printing a bullet list from iterables

--- utils/printing.py
+++ utils/printing.py
@@ -8,4 +8,11 @@ def print_bad(*args, **kwargs):
     return print(SYM_BAD, *args, **kwargs)
 
 
+def itemize(iterable, bullet="•"):
+    if not bullet.endswith(" "):
+        bullet += " "
+    lines = (bullet + str(i) for i in iterable)
+    return "\n".join(lines)
+
+
 
