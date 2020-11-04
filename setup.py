96ef0687efce3ec59cae574974faaa6f4a987ad1
Sven Augustin <sven.augustin@psi.ch>
2020-11-04 15:37:20 +0100

    Release v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sanipy",
-    version="0.0.1",
+    version="0.0.2",
     url="https://gitlab.psi.ch/augustin_s/sanipy",
     description="A command-line tool for epics connection testing",
     author="Paul Scherrer Institute",
